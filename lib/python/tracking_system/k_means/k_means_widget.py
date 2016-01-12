3d14da01f9c03325c16424f1fb3e5b609368f16f
Licht Takeuchi <b116240@hiroshima-u.ac.jp>
2016-01-13 01:54:50 +0900

    Added the k-means centroids estimator

---
+++ lib/python/tracking_system/k_means/k_means_widget.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn import cluster
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+
+try:
+    from .ui_kmeans_widget import Ui_Kmeans_widget
+except ImportError:
+    from ui_kmeans_widget import Ui_Kmeans_widget
+
+class Widget(Ui_Kmeans_widget, QtWidgets.QWidget):
+    reset = pyqtSignal()
+    restart = pyqtSignal()
+
+    def __init__(self, parent):
+        super(Widget, self).__init__(parent)
+        self.setupUi()
+        self.estimator_init()
+
+    def setupUi(self):
+        super(Widget, self).setupUi(self)
+
+        self.resetButton.pressed.connect(self.reset_button_pressed)
+
+    def estimator_init(self):
+        self.k_means = None
+
+    def reset_estimator(self, kv):
+        pass
+
+    def get_name(self):
+        return 'K-means (w/o tracking)'
+
+    def get_tracking_n(self):
+        return self.nObjectsSpinBox.value()
+
+    def get_attributes(self):
+        return {'position':('x', 'y')}
+
+    def track(self, original_img, filtered_img):
+        n_objects = self.nObjectsSpinBox.value()
+
+        if self.k_means is None:
+            self.k_means = cluster.KMeans(n_clusters=n_objects)
+
+        non_zero_pos = np.transpose(np.nonzero(filtered_img.T))
+        center_pos = self.k_means.fit(non_zero_pos).cluster_centers_
+
+        return {'position': center_pos}
+
+    @pyqtSlot()
+    def reset_button_pressed(self):
+        self.estimator_init()
+        self.reset.emit()
+
---
+++ lib/python/tracking_system/k_means/ui_kmeans_widget.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file '.\ui_kmeans_widget.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Kmeans_widget(object):
+    def setupUi(self, Kmeans_widget):
+        Kmeans_widget.setObjectName("Kmeans_widget")
+        Kmeans_widget.resize(221, 569)
+        self.verticalLayout_5 = QtWidgets.QVBoxLayout(Kmeans_widget)
+        self.verticalLayout_5.setObjectName("verticalLayout_5")
+        self.verticalLayout_4 = QtWidgets.QVBoxLayout()
+        self.verticalLayout_4.setObjectName("verticalLayout_4")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(Kmeans_widget)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.nObjectsSpinBox = QtWidgets.QSpinBox(Kmeans_widget)
+        self.nObjectsSpinBox.setMinimum(1)
+        self.nObjectsSpinBox.setMaximum(9999)
+        self.nObjectsSpinBox.setObjectName("nObjectsSpinBox")
+        self.horizontalLayout.addWidget(self.nObjectsSpinBox)
+        self.verticalLayout_4.addLayout(self.horizontalLayout)
+        self.horizontalLayout_4 = QtWidgets.QHBoxLayout()
+        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout_4.addItem(spacerItem)
+        self.resetButton = QtWidgets.QPushButton(Kmeans_widget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.resetButton.sizePolicy().hasHeightForWidth())
+        self.resetButton.setSizePolicy(sizePolicy)
+        self.resetButton.setObjectName("resetButton")
+        self.horizontalLayout_4.addWidget(self.resetButton)
+        self.verticalLayout_4.addLayout(self.horizontalLayout_4)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.verticalLayout_4.addItem(spacerItem1)
+        self.verticalLayout_5.addLayout(self.verticalLayout_4)
+
+        self.retranslateUi(Kmeans_widget)
+        QtCore.QMetaObject.connectSlotsByName(Kmeans_widget)
+
+    def retranslateUi(self, Kmeans_widget):
+        _translate = QtCore.QCoreApplication.translate
+        Kmeans_widget.setWindowTitle(_translate("Kmeans_widget", "Form"))
+        self.label.setText(_translate("Kmeans_widget", "# of objects"))
+        self.resetButton.setText(_translate("Kmeans_widget", "Set/Reset"))
+
