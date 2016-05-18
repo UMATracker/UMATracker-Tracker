46d38e125b12dcee3305245368c3daf3a83fa9c9
Licht Takeuchi <licht-t@math.dis.titech.ac.jp>
2016-05-19 06:04:11 +0900

    Added the skeleton and surface estimator

---
+++ lib/python/tracking_system/group_tracker_with_skeleton/group_tracker_widget.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn import cluster
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
+
+try:
+    from .ui_group_tracker_widget import Ui_group_tracker_widget
+except ImportError:
+    from ui_group_tracker_widget import Ui_group_tracker_widget
+
+try:
+    from .group_tracker import GroupTrackerGMM
+except ImportError:
+    from group_tracker import GroupTrackerGMM
+
+from mvpa2.suite import *
+import matplotlib.pyplot as plt
+
+
+import shapely.geometry as geometry
+from shapely.ops import cascaded_union, polygonize, unary_union
+from descartes import PolygonPatch
+import networkx as nx
+
+from scipy.interpolate import splev, splprep
+from scipy.spatial import Delaunay
+from scipy.spatial import Voronoi, voronoi_plot_2d
+import numpy as np
+import math
+
+import skeletonize2D
+
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import minimum_spanning_tree
+
+
+def get_longest_paths(nodes, graph):
+    """
+    Returns longest path of all possible paths between a list of nodes.
+    """
+    paths = []
+    distances = []
+    possible_paths = list(combinations(nodes, r=2))
+    for node1, node2 in possible_paths:
+        try:
+            path = nx.shortest_path(graph, node1, node2, "weight")
+        except:
+            path = []
+        if len(path)>1:
+            distance = get_path_distance(path, graph)
+            paths.append(path)
+            distances.append(distance)
+    paths_sorted = [x for (y,x) in sorted(zip(distances, paths), reverse=True)]
+    return paths_sorted
+
+def get_path_distance(path, graph):
+    """
+    Returns weighted path distance.
+    """
+    distance = 0
+    for i,w in enumerate(path):
+        j=i+1
+        if j<len(path):
+            distance += round(graph.edge[path[i]][path[j]]["weight"], 6)
+    return distance
+
+class Widget(Ui_group_tracker_widget, QtWidgets.QWidget):
+    reset = pyqtSignal()
+    restart = pyqtSignal()
+
+    def __init__(self, parent):
+        super(Widget, self).__init__(parent)
+        self.setupUi()
+        self.estimator_init()
+
+    def setupUi(self):
+        super(Widget, self).setupUi(self)
+
+        self.resetButton.pressed.connect(self.reset_button_pressed)
+        self.restartButton.pressed.connect(self.restart_button_pressed)
+        self.nObjectsSpinBox.valueChanged.connect(self.n_objects_spinbox_value_changed)
+
+        self.likelihoodDiffThresholdSpinBox.valueChanged.connect(self.likelihoodDiffThresholdSpinBoxValueChanged)
+
+    def likelihoodDiffThresholdSpinBoxValueChanged(self, val):
+        if self.gmm is not None:
+            self.gmm.set_likelihood_diff_threshold(val)
+
+    def estimator_init(self):
+        self.gmm = None
+        self.prev_contours = None
+
+    def reset_estimator(self, kv):
+        if self.gmm is not None:
+            self.gmm.means_[:] = kv['position']
+            self.gmm.params = 'wc'
+
+    def get_name(self):
+        return 'Group Tracker with skeleton estimator'
+
+    def get_tracking_n(self):
+        return self.nObjectsSpinBox.value()
+
+    def get_attributes(self):
+        return {'position':('x', 'y'), 'path':None, 'polygon':None}
+
+    def track(self, original_img, filtered_img):
+        n_objects = self.nObjectsSpinBox.value()
+        n_k_means = self.nKmeansSpinBox.value()
+
+        non_zero_pos = np.transpose(np.nonzero(filtered_img.T))
+
+        if self.prev_contours is None:
+            self.prev_contours = [None for i in range(n_objects)]
+
+        # FIXME: 真っ黒な画像が入力されたときのためアドホックに対処．
+        if self.gmm is None:
+            self.gmm = GroupTrackerGMM(n_components=n_objects, covariance_type='full', n_iter=1000, init_params='wc', params='wc')
+            self.gmm.set_likelihood_diff_threshold(self.likelihoodDiffThresholdSpinBox.value())
+
+        try:
+            self.gmm._fit(non_zero_pos, n_k_means=n_k_means)
+            self.res = self.gmm.means_
+        except:
+            pass
+
+        predict = self.gmm.predict(non_zero_pos)
+        skeletons = []
+        for i in range(n_objects):
+            p_data = non_zero_pos[predict == i]
+
+            ch_vtx_list = skeletonize2D.get_concave_hull(p_data.astype(np.int32))
+            ch_poly = unary_union(list(polygonize(geometry.MultiLineString(ch_vtx_list))))
+            if ch_poly.type == 'Polygon':
+                ch = np.array(ch_poly.exterior.coords)
+            elif ch_poly.type == 'MultiPolygon':
+                try:
+                    ch = np.array(cascaded_union(ch_poly.buffer(5)).exterior.coords)
+                except:
+                    ch = self.prev_contours[i]
+
+            self.prev_contours[i] = ch
+
+            edges, vertices = skeletonize2D.get_skeleton_from_polygon(ch.astype(np.float64))
+            G=nx.Graph()
+            G.add_weighted_edges_from(edges)
+
+            init_nodes = list(filter(lambda n:G.degree(n)==1, G.nodes_iter()))
+            longest_path_list = get_longest_paths(init_nodes, G)
+
+            optimal_path = skeletonize2D.find_optimal_path(longest_path_list, vertices)
+            skeletons.append(vertices[optimal_path[1:-1], :])
+            # tck, u = splprep([unique[path, 0], unique[path, 1]])
+            # t = 0.0
+            # t_list = []
+            # while t<1:
+            #     t_list.append(t)
+            #     t += 1.0/ np.linalg.norm(splev(t, tck, der=1))
+            #
+            # interpolated = splev(t_list,tck)
+            # skeletons.append(np.array([[x,y] for x,y in zip(*interpolated)]))
+
+            # plt.plot(unique[path, 0], unique[path, 1])
+            # plt.scatter(interpolated[0], interpolated[1])
+
+        # plt.show()
+
+        return {'position': self.res, 'path':skeletons, 'polygon':self.prev_contours}
+
+    @pyqtSlot()
+    def reset_button_pressed(self):
+        self.estimator_init()
+        self.reset.emit()
+
+    @pyqtSlot()
+    def restart_button_pressed(self):
+        self.restart.emit()
+
+    @pyqtSlot(int)
+    def n_objects_spinbox_value_changed(self, i):
+        self.nKmeansSpinBox.setMinimum(i)
